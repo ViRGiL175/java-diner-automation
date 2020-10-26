baa3f79c2f367fead7ec698dc0c39835df7ebc14
ViRGiL175 <virgil7.dev@gmail.com>
2020-10-26 18:29:28 +0400

    Fix Java SDK and add Gradle encodings fix, Gson added

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,13 @@ repositories {
     mavenCentral()
 }
 
+// Fix for encoding problems
+tasks.withType<JavaCompile> {
+    options.encoding = "windows-1251"
+}
+
 dependencies {
     testImplementation("junit", "junit", "4.12")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("com.google.code.gson:gson:2.8.6")
 }
